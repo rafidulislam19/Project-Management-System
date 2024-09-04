a5d9abbaa8b0bcb9960de7bdf5c02da3881eb6ee
rafidulislam19 <you@example.com>
2024-09-04 17:41:02 +0600

    project, task, todo

--- project_management_system/project_management_system/urls.py
+++ project_management_system/project_management_system/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('account.urls')),
     path('projects/', include('project.urls')),
+    path('projects/<uuid:project_id>/', include('todolist.urls')),
+    path('projects/<uuid:project_id>/<uuid:todolist_id>/', include('task.urls')),
 ]
